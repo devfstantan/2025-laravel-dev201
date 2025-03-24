34e98f718aa9014147390b256582cb13e08097a7
DEVFS <devfs.tantan@gmail.com>
2025-03-24 12:05:52 +0000

    database: query builder

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -3,22 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProduitController extends Controller
 {
     public function listProduits(){
-        $produits = [
-            ['title' => 'Produit1', 'price' => 100, 'stock' => 10],
-            ['title' => 'Produit2', 'price' => 10, 'stock' => 0],
-            ['title' => 'Produit3', 'price' => 200, 'stock' => 9],
-            ['title' => 'Produit3', 'price' => 200, 'stock' => 100],
-            ['title' => 'Produit3', 'price' => 200, 'stock' => 100],
-            ['title' => 'Produit3', 'price' => 200, 'stock' => 100],
-            ['title' => 'Produit3', 'price' => 200, 'stock' => 100],
-            ['title' => 'Produit3', 'price' => 200, 'stock' => 100],
-            ['title' => 'Produit3', 'price' => 200, 'stock' => 100],
-            ['title' => 'Produit3', 'price' => 200, 'stock' => 100],
-        ];
+        $produits = DB::table('produits')
+        // ->select(['title','price'])
+        ->where('price','<', 100)
+        ->orWhere('price', '>=', 1000)
+        // ->orderBy('price','desc')
+        ->latest('price')
+        ->get();
+        dd($produits);
+
+        // $data = DB::table('produits')
+        // ->selectRaw('count(*) as nb_produits , categorie_id')
+        // ->where('price', '>=', 200)
+        // ->groupBy('categorie_id')
+        // ->having('nb_produits','>=',10)
+        // ->get();
+        
+        // dd($data);
     
         return view('produits.index', compact('produits'));
     }
