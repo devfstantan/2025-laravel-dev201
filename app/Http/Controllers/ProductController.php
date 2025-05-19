77ced6e7c618ab31c7078fc574b73150a55113a7
DEVFS <devfs.tantan@gmail.com>
2025-05-19 09:20:35 +0100

    fixed product edit page bug

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -62,7 +62,9 @@ class ProductController extends Controller
     public function edit(string $id)
     {
         $product = Product::findOrFail($id);
-        return view('products.edit', compact('product'));
+        $categories = Category::orderBy('name')->get();
+
+        return view('products.edit', compact('product','categories'));
     }
 
     /**
